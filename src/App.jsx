816368f43769f5dee61a2c63bdd33ea66288bf82
Nikolai Petrov <nikolayuy@gmail.com>
2024-04-10 12:29:49 +0300

    init app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+	return (
+		<>
+			<div className="text-blue-700">App</div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi soluta,
+				voluptas asperiores accusantium fugit corrupti? Perspiciatis,
+				asperiores. Architecto rerum illum atque eligendi culpa assumenda nihil
+				dolorum, veritatis error, libero reiciendis?
+			</p>
+		</>
+	);
+}
